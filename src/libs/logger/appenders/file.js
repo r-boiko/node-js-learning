50a1db57efe91c44fee5725323f6495923fa029c
Roman Boiko <rboi.personal@gmail.com>
2024-02-27 16:22:08 +0200

    added async for file appender

--- src/libs/logger/appenders/file.js
+++ src/libs/logger/appenders/file.js
@@ -1,16 +1,16 @@
-import fs from 'fs';
+import { appendFile } from 'node:fs/promises';
 import { formatMessage } from '../utils/formatMessage.js';
 import { LEVEL } from '../constants.js';
 
-const log = (date, level, category, message) => {
-  fs.appendFileSync(
+const log = async (date, level, category, message) => {
+  await appendFile(
     './logs/app.txt',
     formatMessage(date, level, category, message, '\n'),
     'utf-8',
   );
 
   if (level === LEVEL.ERROR) {
-    fs.appendFileSync(
+    await appendFile(
       './logs/app_error.txt',
       formatMessage(date, level, category, message, '\n'),
       'utf-8',
